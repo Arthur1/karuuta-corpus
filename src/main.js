92034a02ea0033e171af91201028fc7305006ed8
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2020-08-31 12:57:27 +0900

    歌詞DL

--- src/main.js
+++ src/main.js
@@ -1,3 +1,46 @@
-'use struct'
+import cliProgress from 'cli-progress'
+import Song from './song.js'
 
-console.log('Hello World')
+main().then(() => {}).catch(err => {
+  console.error(err)
+})
+
+async function main() {
+  const songs = [
+    {
+      artist: 'YOASOBI',
+      title: '夜に駆ける'
+    },
+    {
+      artist: 'ずっと真夜中でいいのに。',
+      title: 'お勉強しといてよ'
+    },
+    {
+      artist: '日向電工',
+      title: 'ブリキノダンス'
+    }
+  ]
+  const songsNumber = songs.length
+  const progressBar = new cliProgress.SingleBar({
+    format: 'Download Lyrics |' + '{bar}' + '| {percentage}% || {value}/{total} || {song}',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591',
+    hideCursor: true
+  })
+  progressBar.start(songsNumber, 0, {
+    song: `${songs[0].title} - ${songs[0].artist}`
+  })
+
+  for (let i = 0; i < songsNumber; i++) {
+    const currentSong = songs[i]
+    const nextSong = songs[i + 1] || null
+    const songObj = new Song(currentSong.artist, currentSong.title)
+    // Promise.all()を利用したほうが効率的だが、スクレイピングの間隔制御ができない
+    await songObj.fetchLyric()
+    progressBar.increment({
+      song: nextSong ? `${nextSong.title} - ${nextSong.artist}`  : 'Finished'
+    })
+  }
+
+  progressBar.stop()
+}
---
+++ src/song.js
@@ -0,0 +1,101 @@
+import axios from 'axios'
+import jsdom from 'jsdom'
+import fs from 'fs/promises'
+import path from 'path'
+import sanitize from 'sanitize-filename'
+import deepcopy from 'deepcopy'
+import sleep from './sleep.js'
+
+const { JSDOM } = jsdom
+axios.defaults.baseURL = 'https://utaten.com/'
+
+export default class Song {
+  constructor(artist, title) {
+    this.artist = artist
+    this.title = title
+    this.url = null
+    this.htmlLyricPath = path.join('resources', 'html_lyrics', sanitize(`${this.artist}___${this.title}.html`))
+    this.rawLyricPath = path.join('resources', 'raw_lyrics', sanitize(`${this.artist}___${this.title}.txt`))
+  }
+
+  /**
+   * 歌詞のURLを取得
+   * @async
+   */
+  async fetchUrl() {
+    const url = '/lyric/search'
+    const options = {
+      params: {
+        sort: 'popular_sort',
+        artist_name: this.artist,
+        title: this.title
+      },
+      responseType: 'document'
+    }
+    const res = await axios.get(url, options)
+    await sleep(1000)
+    const dom = new JSDOM(res.data)
+    this.url = dom.window.document.querySelector('.lyricList__beginning > a').getAttribute('href')
+  }
+
+  /**
+   * 歌詞を取得してダウンロード
+   * @async
+   */
+  async fetchLyric() {
+    if (await this.rawLyricExists()) return
+    if (! this.url) await this.fetchUrl()
+    const options = {
+      responseType: 'document'
+    }
+    const res = await axios.get(this.url, options)
+    await sleep(1000)
+    const dom = new JSDOM(res.data)
+    const lyricDom = dom.window.document.querySelector('.hiragana')
+    await Promise.all([this.saveHtmlLyric(lyricDom), this.saveRawLyric(lyricDom)])
+  }
+
+  /**
+   * HTML版歌詞(読みを含む)を書き出し
+   * @async
+   * @param {Element} lyricDom  歌詞要素のDOM Node
+   */
+  async saveHtmlLyric(lyricDom) {
+    const htmlLyric = lyricDom.innerHTML.trim()
+    await fs.writeFile(this.htmlLyricPath, htmlLyric)
+  }
+
+  /**
+   * テキスト版歌詞を書き出し
+   * @async
+   * @param {Element} lyricDom  歌詞要素のDOM Node
+   */
+  async saveRawLyric(lyricDom) {
+    const clonedLyricDom = deepcopy(lyricDom)
+    clonedLyricDom.querySelectorAll('.rt').forEach(el => el.innerHTML = '')
+    const rawLyric = clonedLyricDom.textContent.trim()
+    await fs.writeFile(this.rawLyricPath, rawLyric)
+  }
+
+  /**
+   * テキスト版歌詞が存在するかどうか
+   * @async
+   * @return {Promise<Boolean>}
+   */
+  async rawLyricExists() {
+    try {
+      await this.loadRawLyric()
+      return true
+    } catch (err) {
+      return false
+    }
+  }
+
+  /**
+   * テキスト版歌詞をファイルから読み込む
+   * @async
+   */
+  async loadRawLyric() {
+    await fs.readFile(this.rawLyricPath)
+  }
+}
